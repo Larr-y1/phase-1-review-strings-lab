2837aa8371973052d38a99a59825cce19fbd8fd5
Larry Otieno <larryotieno51@gmail.com>
2025-03-05 22:55:44 +0300

    Adds string labs phase 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+
+const shortGreeting = `Welcome, ${currentUser[0]} !`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
+
+
